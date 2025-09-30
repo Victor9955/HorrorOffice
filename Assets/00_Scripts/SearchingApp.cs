2829130e0fb5424b176e7fbbc34e597c19c2052d
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2025-09-30 16:02:03 +0200

    EDIT SearchingApp

--- Assets/00_Scripts/SearchingApp.cs
+++ Assets/00_Scripts/SearchingApp.cs
@@ -1,26 +1,29 @@
 using DG.Tweening;
+using NaughtyAttributes;
 using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
-[Serializable]
-class Page
-{
-    public string pageName;
-    public Transform pageTransform;
-}
 
 public class SearchingApp : MonoBehaviour, IApp
 {
     [SerializeField] List<Transform> toShow;
-    [SerializeField] List<Page> values;
-    RectTransform rec;
+    [SerializeField] int size;
+    [SerializeField] RectTransform rec;
+    [SerializeField] RectTransform contentAncor;
+    [SerializeField] GridLayoutGroup content;
+    [SerializeField] RectTransform idPrefab;
+    [SerializeField] TMP_InputField inputField;
+    Dictionary<string, GameObject> codes = new();
+
     private void Start()
     {
-        rec = GetComponent<RectTransform>();
-
+        contentAncor.sizeDelta = new Vector2(0,size * content.cellSize.y);
+        RandomCode(size);
     }
-
 
     public void Open()
     {
@@ -31,5 +34,67 @@ public class SearchingApp : MonoBehaviour, IApp
     public void Close()
     {
         rec.DOScale(0f, 0.3f).OnComplete(() => toShow.ForEach(show => show.gameObject.SetActive(false)));
+        Searching("");
+        inputField.text = "";
+    }
+
+    void RandomCode(int amount)
+    {
+        int interval = (61439 / (amount * 4));
+        string str = "";
+        int j = 0;
+        for (int i = 4096; i + interval <= 65535; i += interval)
+        {
+            int randomNumber = UnityEngine.Random.Range(i, i + interval);
+            if(j == 3)
+            {
+                GameObject cash = Instantiate(idPrefab.gameObject, contentAncor);
+                cash.GetComponentInChildren<TextMeshProUGUI>().text = str;
+                codes.Add(str, cash);
+                j = 0;
+                str = "";
+            }
+            else
+            {
+                if(j == 0)
+                {
+                    str += Convert.ToString(randomNumber, 16);
+                }
+                else
+                {
+                    str += ":" + Convert.ToString(randomNumber, 16);
+                }
+                j++;
+            }
+        }
+    }
+
+    public void Searching(string input)
+    {
+        if (input == "")
+        {
+            foreach (GameObject obj in codes.Values)
+            {
+                obj.SetActive(true);
+            }
+            contentAncor.sizeDelta = new Vector2(0, size * content.cellSize.y);
+        }
+        else
+        {
+            int counter = 0;
+            foreach (string str in codes.Keys)
+            {
+                if (str.Contains(input, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    codes[str].SetActive(true);
+                    counter++;
+                }
+                else
+                {
+                    codes[str].SetActive(false);
+                }
+            }
+            contentAncor.sizeDelta = new Vector2(0, counter * content.cellSize.y);
+        }
     }
 }
