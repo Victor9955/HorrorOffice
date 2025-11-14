4d6d55e977323110f36aba0c805a0c1cbf275940
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2025-11-14 16:18:02 +0100

    ADD Gameplay Event

--- Assets/00_Scripts/Data/GameplayEventSender.cs
+++ Assets/00_Scripts/Data/GameplayEventSender.cs
@@ -7,4 +7,12 @@ public class GameplayEventSender : ScriptableObject
 {
     public Action<MailData> OnSendMail;
     public void SendMail(MailData mail) => OnSendMail?.Invoke(mail);
+
+    [SerializeField] Vector2 mailRandomWait;
+
+    public async void SendMailWait(MailData mail)
+    {
+        await Awaitable.WaitForSecondsAsync(UnityEngine.Random.Range(mailRandomWait.x, mailRandomWait.y));
+        SendMail(mail);
+    }
 }
