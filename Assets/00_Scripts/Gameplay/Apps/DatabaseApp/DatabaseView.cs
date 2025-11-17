638bf89849e43c892294ba08183a77b3514163d4
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2025-11-17 14:53:37 +0100

    EDIT Database integration

--- Assets/00_Scripts/Gameplay/Apps/DatabaseApp/DatabaseView.cs
+++ Assets/00_Scripts/Gameplay/Apps/DatabaseApp/DatabaseView.cs
@@ -12,6 +12,14 @@ public class DatabaseView : MonoBehaviour
         _characterName.text = data.characterName;
         _characterIdTMP.text = data.charachterId;
         _testData.text = "A fait " + data.pausesNumber.ToString() + " pauses";
-
+        //public string beginTime;
+        //public string breakfastTime;
+        //[TextArea(1, 20)] public string medicineTime;
+        //public string HGrade;
+        //public string EGrade;
+        //public string AGrade;
+        //public string LGrade;
+        //public string TGrade;
+        //public string HGradeTwo;
     }
 }
