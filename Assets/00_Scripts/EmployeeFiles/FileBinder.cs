57f90336ea6cfb55836fcf04addbc97706bccc8f
CamilleS <cs.sabycamille@gmail.com>
2025-11-21 21:07:49 +0100

    [AUDIO] Add Sorter Sound

--- Assets/00_Scripts/EmployeeFiles/FileBinder.cs
+++ Assets/00_Scripts/EmployeeFiles/FileBinder.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using FMODUnity;
 using System;
 using System.Collections;
 using TMPro;
@@ -10,6 +11,7 @@ public class FileBinder : MonoBehaviour, IDropContainer
 {
     [SerializeField] private Transform _childContainerTR;
     [SerializeField] private TMP_Text _text;
+    [SerializeField] private EventReference _hoverSound;
     public Binder BinderType => _binderType;
     public bool animIsOpen;
     public bool isUnlocked;
@@ -60,7 +62,7 @@ public class FileBinder : MonoBehaviour, IDropContainer
 
     private void OnMouseEnter()
     {
-        UpdateOpenState(true);
+        UpdateOpenState(true, true);
     }
 
     private void OnMouseExit()
@@ -96,6 +98,10 @@ public class FileBinder : MonoBehaviour, IDropContainer
         {
             if (_animRoutine != null) StopCoroutine(_animRoutine);
             _animRoutine = StartCoroutine(OpenCoroutine());
+            if (isOpening && !_hoverSound.IsNull)
+            {
+                RuntimeManager.PlayOneShot(_hoverSound, transform.position);
+            }
         }
     }
 
