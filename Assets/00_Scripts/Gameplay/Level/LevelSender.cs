98bc68468a9dd77391b613c53cb4ff410e68f138
Maxens1011 <MaxensThiam@outlook.com>
2025-11-25 15:43:04 +0100

    Revert "Merge remote-tracking branch 'origin/GP_Victor' into GP_Maxens"
    This reverts commit 5227c061604420101240bc7e5041f4bbb9c658e9, reversing
    changes made to 47739e2f48fbb04bacd44455ac9bdef4fcf9f7c0.

--- Assets/00_Scripts/Gameplay/Level/LevelSender.cs
+++ Assets/00_Scripts/Gameplay/Level/LevelSender.cs
@@ -1,15 +1,10 @@
-using DG.Tweening;
 using HuntroxGames.Utils;
 using NaughtyAttributes;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
-using UnityEngine.SceneManagement;
 
 public class LevelSender : MonoBehaviour
 {
@@ -18,35 +13,20 @@ public class LevelSender : MonoBehaviour
     [SerializeField] private Vector2 randomWaitTimeForCharacter;
     [SerializeField] List<DayData> days;
 
-    [Header("We are in the End game now")]
-    [SerializeField] bool beginFirstDay;
-    [SerializeField] SceneAsset endGameScene;
-    [SerializeField] Volume volume;
-    [SerializeField] float vignetteTime = 0.25f;
-
-    static int day;
+    int day;
     DayData current;
-    Vignette vignette = null;
 
     public event Action<DayData> OnBeginDay;
     public event Action OnEndDay;
     public event Action OnEndGame;
 
-    private void Start()
+    [ConsoleCommand("BeginDay", "[Integer Input]")]
+    public void BeginDay(int m_day)
     {
-        if(beginFirstDay)
-        {
-            day = 0;
-        }
-        volume.profile.TryGet<Vignette>(out vignette);
-        BeginDay();
-    }
-
-    [ConsoleCommand("BeginDay")]
-    public void BeginDay()
-    {
+        if(m_day > days.Count -1 || m_day < 0) return;
         if (current == null) // when current = null current level is finished
         {
+            day = m_day;
             current = days[day];
             current.startEvent?.Invoke();
             fileSorting._binderDataList = current.binders;
@@ -73,27 +53,14 @@ public class LevelSender : MonoBehaviour
             yield return new WaitUntil(() => levelCreator.isEnded);
             yield return new WaitForSeconds(UnityEngine.Random.Range(randomWaitTimeForCharacter.x, randomWaitTimeForCharacter.y));
         }
-        day = Mathf.Clamp(day + 1, 0, days.Count - 1);
-        if (day == days.Count -1)
+        current = null;
+        if(day == days.Count -1)
         {
             OnEndGame?.Invoke();
-            EndDay();
         }
         else
         {
             OnEndDay?.Invoke();
-            SceneManager.LoadScene(endGameScene.name);
         }
-        current = null;
-    }
-
-    [Button("End Day Test")]
-    void EndDay()
-    {
-        vignette.intensity.max = 1000f;
-        DOTween.To(() => vignette.intensity.value, (i) => vignette.intensity.value = i, vignette.intensity.max, vignetteTime).OnComplete(() =>
-        {
-            SceneManager.LoadScene(current.endDayScene.name);
-        });
     }
 }
