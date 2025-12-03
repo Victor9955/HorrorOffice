35501b68f0819503126f40fdc97cfe769ef598e2
Maxens1011 <MaxensThiam@outlook.com>
2025-12-03 12:03:56 +0100

    [EDIT] Main Menu redoing lights
    it exploded :(

--- Assets/00_Scripts/UI/UIMainMenu.cs
+++ Assets/00_Scripts/UI/UIMainMenu.cs
@@ -10,21 +10,18 @@ public class UIMainMenu : MonoBehaviour
 {
     [SerializeField] private Transform _leftDoorTR;
     [SerializeField] private Transform _rightDoorTR;
-    [SerializeField] private List<Light> _lights = new();
+    [SerializeField] private Light _light;
     [Space]
     [SerializeField] private float _openDistance;
     [SerializeField] private float _openDuration;
 
     private int _sceneIndex;
-    private List<float> _initLightIntens = new();
+    private float _initLightIntens;
 
     private void Start()
     {
-        for (int i = 0; i < _lights.Count; i++)
-        {
-            _initLightIntens.Add(_lights[i].intensity);
-            _lights[i].intensity = 0;
-        }
+            _initLightIntens = _light.intensity;
+            _light.intensity = 0;
 
 
     }
@@ -33,8 +30,7 @@ public class UIMainMenu : MonoBehaviour
         _sceneIndex = index;
         _leftDoorTR.DOMove(_leftDoorTR.position + (_leftDoorTR.up * _openDistance), _openDuration);
         _rightDoorTR.DOMove(_rightDoorTR.position + (-_rightDoorTR.up * _openDistance), _openDuration);
-            DOVirtual.Float(0, _initLightIntens[0], _openDuration, (intens) => _lights[0].intensity = intens);
-            DOVirtual.Float(0, _initLightIntens[0], _openDuration, (intens) => _lights[1].intensity = intens);
+            DOVirtual.Float(0, _initLightIntens, _openDuration, (intens) => _light.intensity = intens);
 
         Invoke("LaunchScene", 2);
     }
