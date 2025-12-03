dbf7bdc6417f2e3b6fa6681ed92f4233e3c4958d
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2025-12-03 15:50:42 +0100

    PUSH MainScene

--- Assets/00_Scripts/Gameplay/Apps/MailApp/Mail.cs
+++ Assets/00_Scripts/Gameplay/Apps/MailApp/Mail.cs
@@ -21,7 +21,7 @@ public class Mail : MonoBehaviour
         transform.localScale = Vector3.zero;
         transform.DOScale(baseScale, 0.3f);
         titleTMP.text = mailData.title;
-        titleTMP.text += $"\n <color=#{nameColor.ToHexString()}><" + mailData.character.staticInfo.name + ">";
+        titleTMP.text += $"\n <color=#{nameColor.ToHexString()}><size=12.5><" + mailData.character.staticInfo.name + ">";
     }
 
     public void OnClicked()
